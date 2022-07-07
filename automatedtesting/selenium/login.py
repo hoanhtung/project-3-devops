b0d0551ad1a672b7e7b82b3e23ed8fd85c569496
Hoanh Tung <hoanhtung.xy@gmail.com>
2022-07-07 18:02:38 +0700

    config selenium

--- automatedtesting/selenium/login.py
+++ automatedtesting/selenium/login.py
@@ -7,8 +7,8 @@ import datetime
 #driver = webdriver.Chrome()
 options = ChromeOptions()
 options.add_argument('--headless')
-options.add_argument('--no-sandbox')
-options.add_argument('--disable-dev-shm-usage')
+# options.add_argument('--no-sandbox')
+# options.add_argument('--disable-dev-shm-usage')
 driver = webdriver.Chrome('/usr/bin/chromedriver', options=options)
 
 def timestamp():
