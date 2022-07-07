05f3e4e41dffb5aba76e6ac46ee46b2a1a04d7f9
Hoanh Tung <hoanhtung.xy@gmail.com>
2022-07-07 18:16:30 +0700

    config selenium

--- automatedtesting/selenium/login.py
+++ automatedtesting/selenium/login.py
@@ -9,7 +9,7 @@ options = ChromeOptions()
 options.add_argument('--headless')
 # options.add_argument('--no-sandbox')
 # options.add_argument('--disable-dev-shm-usage')
-driver = webdriver.Chrome('/home/tungudacity/usr/bin/chromedriver', options=options)
+driver = webdriver.Chrome('/home/tungudacity/myagent/bin/chromedriver', options=options)
 
 def timestamp():
     return datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
