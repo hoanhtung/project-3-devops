5a119266c4457c9ab506ff6bb3a727d11f5a581f
Hoanh Tung <hoanhtung.xy@gmail.com>
2022-07-07 19:11:29 +0700

    config selenium

--- automatedtesting/selenium/login.py
+++ automatedtesting/selenium/login.py
@@ -8,7 +8,7 @@ import datetime
 options = ChromeOptions()
 options.add_argument('--headless')
 options.add_argument('--no-sandbox')
-options.add_argument('--remote-debugging-port=9222')
+options.add_argument('--remote-debugging-port=9223')
 options.add_argument('--disable-dev-shm-usage')
 driver = webdriver.Chrome(options=options)
 
