749568c55545046e8fb6fdb1e68a2774fbcf0f03
hoanhtung <hoanhtung.xy@gmail.com>
2022-07-08 12:27:58 +0700

    Update login.py

--- automatedtesting/selenium/login.py
+++ automatedtesting/selenium/login.py
@@ -7,9 +7,9 @@ import datetime
 #driver = webdriver.Chrome()
 options = ChromeOptions()
 options.add_argument('--headless')
-options.add_argument('--no-sandbox')
-options.add_argument('--remote-debugging-port=9223')
-options.add_argument('--disable-dev-shm-usage')
+# options.add_argument('--no-sandbox')
+# options.add_argument('--remote-debugging-port=9223')
+# options.add_argument('--disable-dev-shm-usage')
 driver = webdriver.Chrome(options=options)
 
 def timestamp():
